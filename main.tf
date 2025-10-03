a1a64679c790337090c3816bac637fa803868ef8
Tyler <rylertobinson123@gmail.com>
2025-10-04 00:24:55 +0100

    resolving terraform formatting failure

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my_bucket" {
-  name          = "github-action-gcs123"
-  location      = "EU"
-  force_destroy = true
+  name                     = "github-action-gcs123"
+  location                 = "EU"
+  force_destroy            = true
   public_access_prevention = true
 }
