8e78ef834c7d7b3887e510c72fe27d8cdd9d578c
Tyler <rylertobinson123@gmail.com>
2025-10-04 00:29:42 +0100

    bucket name edit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my_bucket" {
-  name                     = "github-action-gcs123"
+  name                     = "github-action-gcs123_new"
   location                 = "EU"
   force_destroy            = true
   public_access_prevention = true
