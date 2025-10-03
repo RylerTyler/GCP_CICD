87a9b1265ca94d1088548085f43b8ec30f943a1b
Tyler <rylertobinson123@gmail.com>
2025-10-04 00:21:39 +0100

    bucket name changes

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
 resource "google_storage_bucket" "my_bucket" {
-  name          = "tr_lbg_bucket"
+  name          = "github-action-gcs123"
   location      = "EU"
   force_destroy = true
-
-  uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
+  public_access_prevention = true
+}
