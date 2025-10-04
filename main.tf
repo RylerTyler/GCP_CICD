bcce0a63dbbd9b65d1db976a1d8cb4f366be36ba
Tyler <rylertobinson123@gmail.com>
2025-10-04 11:32:28 +0100

    updating main.tf file to include project number

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
-resource "google_storage_bucket" "my_bucket" {
+provider "google" {
+  project = "lbg123-473910"
+  region     = "europe-west2"
+}
+
+resource "google_storage_bucket" "my_bucket"{
   name                     = "github-action-gcs123_new"
   location                 = "EU"
   force_destroy            = true
-  public_access_prevention = true
+  public_access_prevention = "enforced"
 }
