92e06cc5c2475b72bd0464d88b757dd3161257a7
Tyler <rylertobinson123@gmail.com>
2025-10-04 11:36:18 +0100

    changing terraform formatting

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "google" {
   project = "lbg123-473910"
-  region     = "europe-west2"
+  region  = "europe-west2"
 }
 
-resource "google_storage_bucket" "my_bucket"{
+resource "google_storage_bucket" "my_bucket" {
   name                     = "github-action-gcs123_new"
   location                 = "EU"
   force_destroy            = true
